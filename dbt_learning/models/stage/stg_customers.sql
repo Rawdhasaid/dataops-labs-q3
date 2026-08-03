069a0c0e3d8673b54a3f05d5b563c05235dfafe8
Rawdha Al Yahyai <rawdha.yahyai@rihal.om>
2026-08-03 15:15:10 +0400

    Trying to remove nulls, applied a coalesce

--- dbt_learning/models/stage/stg_customers.sql
+++ dbt_learning/models/stage/stg_customers.sql
@@ -7,8 +7,8 @@ cleaned as (
 	trim(customer_id)::text as customer_id,
 	INITCAP(trim(first_name))::text as first_name,
 	INITCAP(trim(last_name))::text as last_name,
-	lower(email)::text as customer_email,
-	trim(phone)::text as phone_number, 
+	coalesce(trim(email)::text, 'N/A') as customer_email,
+	coalesce(trim(phone)::text, 'N/A') as phone_number,
 	cast(signup_date as date) as signup_date,
 	trim(country)::text as country, 
 	trim(city)::text as city
