4e5a7bb05e1d851c7d9fcfa776a69fc4f233ca27
Rawdha Al Yahyai <rawdha.yahyai@rihal.om>
2026-08-03 16:10:17 +0400

    Fixed an error , Missed : in order id

--- dbt_learning/models/stage/stg_order_items.sql
+++ dbt_learning/models/stage/stg_order_items.sql
@@ -5,7 +5,7 @@ with source as (
 cleaned_order_items as (
 	select 
 	order_item_id::integer as order_item_id,
-	order_id :integer as order_id,
+	order_id ::integer as order_id,
 	trim(product_id)::text as product_id,
 	cast(quantity as integer) as quantity, 
 	cast(coalesce(unit_price, 0) as numeric(12,2)) as unit_price,
