845a51bda862d1812127b3cff5983b59459ae671
Rawdha Al Yahyai <rawdha.yahyai@rihal.om>
2026-08-03 15:47:22 +0400

    Added Orders Stage

---
+++ dbt_learning/models/stage/stg_orders.sql
@@ -0,0 +1,17 @@
+with source as (
+    select * from {{ ref('raw_orders') }}
+),
+
+cleaned_orders as (
+
+    select
+        order_id::integer                               as order_id,
+        trim(customer_id)::text                         as customer_id,
+        order_date::date                                as order_date,
+        lower(trim(status))::text                       as order_status,
+        coalesce(shipping_fee, 0)::numeric(12,2)        as shipping_fee,
+		cast(upper(trim(currency)) as varchar(3))       as currency_code
+    from source
+
+)
+select * from cleaned_orders
