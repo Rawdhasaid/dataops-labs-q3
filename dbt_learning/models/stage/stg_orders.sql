7e4a83c473032bb6b22b9a60c09d3663889b59fe
Rawdha Al Yahyai <rawdha.yahyai@rihal.om>
2026-08-03 22:30:00 +0400

    Fixed the SQL query of the fact, added a missed store id column in orders stage

--- dbt_learning/models/stage/stg_orders.sql
+++ dbt_learning/models/stage/stg_orders.sql
@@ -9,6 +9,7 @@ cleaned_orders as (
         trim(customer_id)::text                         as customer_id,
         order_date::date                                as order_date,
         lower(trim(status))::text                       as order_status,
+        trim(store_id)::text                            as store_id,
         coalesce(shipping_fee, 0)::numeric(12,2)        as shipping_fee,
 		cast(upper(trim(currency)) as varchar(3))       as currency_code
     from source
