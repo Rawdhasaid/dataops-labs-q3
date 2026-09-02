4ff2556df35c918f1107c4c0396e4a4aa811546e
Rawdha Al Yahyai <rawdha.yahyai@rihal.om>
2026-09-02 15:19:04 +0400

    Addded a Rollup CTE

--- dbt_learning/models/dev/fct_orders.sql
+++ dbt_learning/models/dev/fct_orders.sql
@@ -5,17 +5,35 @@ with orders as (
 
 ),
 
-final as (
+order_items_rollup as (
 
     select
         order_id,
-        customer_id,
-        order_date,
-        order_status,
-        store_id,
-        shipping_fee,
-        currency_code
-    from orders
+        count(order_item_id) as num_line_items,
+        sum(quantity) as total_quantity,
+        sum(net_amount) as net_revenue
+    from {{ ref('fct_order_items') }}
+    group by order_id
+
+),
+
+final as (
+
+    select
+        o.order_id,
+        o.customer_id,
+        o.store_id,
+        o.order_date,
+        o.order_status,
+        o.shipping_fee,
+        o.currency_code,
+        r.num_line_items,
+        r.total_quantity,
+        r.net_revenue,
+        r.net_revenue + o.shipping_fee as order_total
+    from orders o
+    left join order_items_rollup r
+        on o.order_id = r.order_id
 
 )
 
